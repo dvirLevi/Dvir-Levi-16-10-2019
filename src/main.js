8e3a50e51aa6ea932bc9b6a3219e6fed8ad0a9b6
dvirLevi <dvirleviapp@gmail.com>
2019-09-27 12:30:26 +0300

    add fortawesome

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 import "../style/global.css"
-import "../style/fontawesome/css/all.css"
+// import "../style/fontawesome/css/all.css"
 
 Vue.config.productionTip = false
 
