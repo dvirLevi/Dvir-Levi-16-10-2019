ba22e3860b2fe35234a704c6e6e388350fbe191f
dvirLevi <dvirleviapp@gmail.com>
2019-09-27 12:20:21 +0300

    add

---
+++ src/components/tool-bar/categories.vue
@@ -0,0 +1,24 @@
+<template>
+    <router-link class="" tag="div" :to="item.link" router-link-active>
+      {{item.text}}
+    </router-link>
+</template>
+
+<script>
+  export default {
+    name: 'categories',
+    props: {
+      item: Object
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
---
+++ src/components/tool-bar/toolBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="center-all-between">
+        <div class="center-all">
+          <img alt="logo" src="@/assets/logo.png">
+          <h1>Weather app</h1>
+        </div>
+        <div class="center-all">
+        <categories v-for="item in categories" :item="item" :key="item.id" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import categories from '@/components/tool-bar/categories.vue'
+
+  export default {
+    name: 'toolBar',
+    components:{
+      categories
+    },
+    props: {
+
+    },
+    data() {
+      return {
+        categories: [{
+            text: 'Home',
+            link: "/",
+            id: 1
+          },
+          {
+            text: 'Favorites',
+            link: "/favorites",
+            id: 2
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    font-family: 'Assistant';
+    font-weight: 200
+  }
+</style>
